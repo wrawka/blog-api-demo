8f566af2f94847043b752bf5e451e5b739b83f11
Anton Lebedev <46112403+wrawka@users.noreply.github.com>
2021-08-26 23:37:41 +0300

    added models

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,11 +4,22 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
+    group = models.ForeignKey("Group", on_delete=models.SET_NULL, blank=True,
+                      related_name="posts", null=True)
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
 
@@ -24,3 +35,21 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='user')
+    following = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='following')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['following', 'user'], name='unique_following'),
+
+        ]
+
+    def __str__(self) -> str:
+        return f'{self.user} >> {self.following}'
+
