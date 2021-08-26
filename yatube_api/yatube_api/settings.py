0e55e324bbc4e47b9cf1d47f81b2e32e1db1f09d
Anton Lebedev <46112403+wrawka@users.noreply.github.com>
2021-08-26 23:36:44 +0300

    added apps and DRF attributes

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
+    'djoser',
     'api',
     'posts',
 ]
@@ -88,6 +90,13 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT', 'Bearer'),
 }
